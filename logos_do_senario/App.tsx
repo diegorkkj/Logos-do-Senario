da0f80e9e23d3fada93beceabf7b8e6c0efa10b8
diegorkkj <diegosenaisp@gmail.com>
2024-04-20 17:49:55 -0300

    Up Inventario

--- logos_do_senario/App.tsx
+++ logos_do_senario/App.tsx
@@ -1,9 +1,10 @@
 import Cadastro from './Screens/Cadastro'
 import RecupSenha from './Screens/RecupSenha';
 import Login from './screens/Login';
+import Patrimonio from './screens/Patrimonio';
 
 const App = () => {
   return (
-    <Login />
+    <Patrimonio />
   )
 }
