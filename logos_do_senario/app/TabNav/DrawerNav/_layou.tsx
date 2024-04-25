6870cad5215ccd8d3c1497fbea79e06ca2d63a93
diegorkkj <diegosenaisp@gmail.com>
2024-04-25 19:40:49 -0300

    Criação do menu inventario

--- logos_do_senario/app/TabNav/DrawerNav/_layou.tsx
+++ logos_do_senario/app/TabNav/DrawerNav/_layou.tsx
@@ -1,17 +1,73 @@
-import { Drawer } from "expo-router/drawer";
-import CustomDrawer from "@components/customDrawer";
-import { MaterialIcons } from "@expo/vector-icons";
+import CustomDrawer from '@components/customDrawer'
+import { Drawer } from 'expo-router/drawer'
+import { MaterialIcons } from '@expo/vector-icons'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import { useColor } from '../../../temas/temas';
+import { opacity } from 'react-native-reanimated/lib/typescript/reanimated2/Colors';
+
 
 export default function Layout() {
+    const cores = useColor()
+
     return (
-        <Drawer drawerContent={(props) => (
-            <CustomDrawer {...props} />
+        <Drawer drawerContent={(...props) => (
+            CustomDrawer(...props)
         )}>
-            <Drawer.Screen name="Home" options={{headerShown: false, drawerIcon: 
-                ({size, color}) => (
-                    <MaterialIcons name="home" size={size} color={color} />
-                )
-            }} />
+            <Drawer.Screen name='Home'
+                options={{
+                    headerShown: false,
+                    drawerItemStyle: { backgroundColor: cores.bgPrimary },
+                    drawerIcon: ({ size, color }) => (
+                        <MaterialIcons name='home' size={size} color={color} />
+                    )
+                }}
+            />
+
+            <Drawer.Screen
+                name='Perfil'
+                options={{
+                    headerShown: true,
+                    drawerLabelStyle: { color: cores.textColorPrimaryVariant },
+                    drawerIcon: () => (
+                        <MaterialCommunityIcons name="account" size={24} color={cores.textColorPrimaryVariant} />
+                    )
+                }}
+            />
+
+            <Drawer.Screen
+                name='Sobre o App'
+                options={{
+                    headerShown: true,
+                    drawerLabelStyle: { color: cores.textColorPrimaryVariant },
+                    drawerIcon: () => (
+                        <Ionicons name="information-circle" size={24} color={cores.textColorPrimaryVariant} />
+                    )
+                }}
+            />
+
+            <Drawer.Screen
+                name='Suporte'
+                options={{
+                    headerShown: true,
+                    drawerLabelStyle: { color: cores.textColorPrimaryVariant },
+                    drawerIcon: () => (
+                        <Ionicons name="chatbox-ellipses" size={24} color={cores.textColorPrimaryVariant} />
+                    )
+                }}
+            />
+
+            <Drawer.Screen
+                name='Sair'
+                options={{
+                    headerShown: true,
+                    drawerLabelStyle: { color: cores.textColorPrimaryVariant },
+                    drawerIcon: () => (
+                        <Ionicons name="exit" size={24} color={cores.textColorPrimaryVariant} />
+                    )
+                }}
+            />
+
         </Drawer>
     );
 }
